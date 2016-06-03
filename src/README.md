158802306c1cb8da6eec731a6d8b60523e4f4584
Vega <osvaldo.vega@hpe.com>
2016-06-03 00:58:50 -0600

    Adding upppercaseme-file-content

---
+++ src/README.md
@@ -0,0 +1,5 @@
+upper_case_me
+
+------------------
+With this module you can uppercase file's content.
+This module create as a test, following this good tutorial [http://www.anupshinde.com/posts/adding-dependencies-data-nodejs-npm-package/]
---
+++ src/lib/uppercaseme-file-content.js
@@ -0,0 +1,37 @@
+"use strict"
+var fs = require('fs');
+var path = require('path');
+
+var program = require('commander');
+
+/* Define configuration for commander */
+program.version('0.1.1')
+    .option('-n, --new', 'Create a test file')
+    .option('--file [filename]', 'Filename to convert')
+    .parse(process.argv);
+
+function copyFile(from, to) {
+    return fs.createReadStream(from).pipe(fs.createWriteStream(to));
+}
+
+function convertThis() {
+
+    if(program.new) {
+        var newfile = path.join(path.dirname(fs.realpathSync(__filename)), '../lib/examples/file.txt');
+        copyFile(newfile, path.join(process.cwd(), "file.txt"));
+    } else if(program.file) {
+        var myfile = program.file;
+        if(fs.existsSync(myfile)) {
+            var content = fs.readFileSync(myfile, 'utf8');
+            fs.writeFileSync(myfile, content.toUpperCase());
+            console.log("Done");
+        } else {
+            console.log("File does not exist - " + myfile);
+            console.log("OR Create a new file with the --new option");
+        }
+    } else {
+        console.log("Pass a file name/path");
+    }
+}
+
+exports.convert = convertThis;
